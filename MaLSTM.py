aeb8ae6363f1fb1dd765fb169bf4f8bca3b025e0
Xinrui Hua <huaxr13@gmail.com>
2018-05-04 15:39:34 -0500

    Update MaLSTM.py

--- MaLSTM.py
+++ MaLSTM.py
@@ -11,19 +11,58 @@ import matplotlib.pyplot as plt
 import pickle
 import itertools
 import datetime
+import os
+from zipfile import ZipFile
+from os.path import exists
+
 from keras.preprocessing.sequence import pad_sequences
 from keras.models import Model, load_model
-from keras.layers import Input, Embedding, LSTM, Merge
+from keras.layers import Input, Embedding, LSTM, Merge, Bidirectional, Dense, Activation, Flatten, Dropout, BatchNormalization
+from keras.layers import dot, subtract, multiply, concatenate, add
 import keras.backend as K
 from keras.optimizers import Adadelta
 
+NUM = 1
+GoogleNews = False
+n_hidden = 50
+gradient_clipping_norm = 1.25
+batch_size = 64
+n_epoch = 30
+
 PATH = '/u/xh3426/cs388/nlp-project/'
-TRAIN_CSV = PATH + 'data/train_fake.csv'
-TEST_CSV = PATH + 'data/train_fake.csv'
-EMBEDDING_FILE = PATH + 'data/GoogleNews-vectors-negative300.bin.gz'
-MODEL_FILE = PATH + 'data/MaLSTM/model.h5'
-HISTORY_FILE = PATH + 'data/MaLSTM/trainHistory.p'
-PRIDICT_FILE = PATH + 'data/MaLSTM/predict.p'
+TRAIN_CSV = PATH + 'data/train.csv'
+TEST_CSV = PATH + 'data/test.csv'
+if GoogleNews:
+    EMBEDDING_FILE_PATH = PATH + 'data/GoogleNews-vectors-negative300.bin.gz'
+else:
+    EMBEDDING_FILE_PATH = '/scratch/cluster/xh3426/nlp/glove.840B.300d.txt'
+    if not exists(EMBEDDING_FILE_PATH):
+        zipfile = ZipFile('/scratch/cluster/xh3426/nlp/glove.840B.300d.zip')
+        zipfile.extract("glove.840B.300d.txt", path='/scratch/cluster/xh3426/nlp/')
+
+
+SAVEPATH = '/scratch/cluster/xh3426/nlp/MaLSTM' + str(NUM)
+if not os.path.exists(SAVEPATH):
+    os.makedirs(SAVEPATH)
+PNGSAVEPATH = PATH + 'MaLSTM' + str(NUM)
+if not os.path.exists(PNGSAVEPATH):
+    os.makedirs(PNGSAVEPATH)
+MODEL_FILE = SAVEPATH + '/model.h5'
+HISTORY_FILE = SAVEPATH + '/trainHistory.p'
+PRIDICT_FILE = SAVEPATH + '/predict.p'
+LOG_FILE = PNGSAVEPATH + '/log.p'
+ACC_PNG = PNGSAVEPATH + '/accuracy.png'
+LOSS_PNG = PNGSAVEPATH + '/loss.png'
+
+
+pickle.dump({
+    'n_hidden': n_hidden,
+    'gradient_clipping_norm': gradient_clipping_norm,
+    'batch_size': batch_size,
+    'n_epoch': n_epoch,
+    'EMBEDDING': GoogleNews
+}, open(LOG_FILE, "wb"))
+
 
 # Load training and test set
 train_df = pd.read_csv(TRAIN_CSV)
@@ -73,35 +112,28 @@ def text_to_word_list(text):
     return text
 
 
-
 # Prepare embedding
 questions_cols = ['question1', 'question2']
 vocabulary = dict()
 inverse_vocabulary = ['<unk>']  # '<unk>' will never be used, it is only a placeholder for the [0, 0, ....0] embedding
-word2vec = KeyedVectors.load_word2vec_format(EMBEDDING_FILE, binary=True)
-
 
 
 # Iterate over the questions only of both training and test datasets
 for dataset in [train_df, test_df]:
     for index, row in dataset.iterrows():
-
         # Iterate through the text of both questions of the row
         for question in questions_cols:
             q2n = []  # q2n -> question numbers representation
             for word in text_to_word_list(row[question]):
-
                 # Check for unwanted words
-                if word in stops and word not in word2vec.vocab:
+                if word in stops:
                     continue
-
                 if word not in vocabulary:
                     vocabulary[word] = len(inverse_vocabulary)
                     q2n.append(len(inverse_vocabulary))
                     inverse_vocabulary.append(word)
                 else:
                     q2n.append(vocabulary[word])
-
             # Replace questions as word to question as number representation
             dataset.set_value(index, question, q2n)
 
@@ -111,12 +143,26 @@ embeddings = 1 * np.random.randn(len(vocabulary) + 1, embedding_dim)  # This wil
 embeddings[0] = 0  # So that the padding will be ignored
 
 
-# Build the embedding matrix
-for word, index in vocabulary.items():
-    if word in word2vec.vocab:
-        embeddings[index] = word2vec.word_vec(word)
+if GoogleNews:
+    word2vec = KeyedVectors.load_word2vec_format(EMBEDDING_FILE_PATH, binary=True)
+    for word, index in vocabulary.items():
+        if word in word2vec.vocab:
+            embeddings[index] = word2vec.word_vec(word)
+    del word2vec
+else:
+    embeddings_index = {}
+    with open(EMBEDDING_FILE_PATH, encoding='utf-8') as f:
+        for line in f:
+            values = line.split(' ')
+            word = values[0]
+            embedding = np.asarray(values[1:], dtype='float32')
+            embeddings_index[word] = embedding
+    for word, index in vocabulary.items():
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            embeddings[index] = embedding_vector
+    del embeddings_index
 
-del word2vec
 
 max_seq_length = max(train_df.question1.map(lambda x: len(x)).max(),
                      train_df.question2.map(lambda x: len(x)).max(),
@@ -146,19 +192,8 @@ Y_validation = Y_validation.values
 for dataset, side in itertools.product([X_train, X_validation, X_test], ['left', 'right']):
     dataset[side] = pad_sequences(dataset[side], maxlen=max_seq_length)
 
-# Make sure everything is ok
-assert X_train['left'].shape == X_train['right'].shape
-assert len(X_train['left']) == len(Y_train)
 
 # Model variables
-n_hidden = 50
-gradient_clipping_norm = 1.25
-batch_size = 64
-n_epoch = 25
-
-def exponent_neg_manhattan_distance(left, right):
-    ''' Helper function for the similarity estimate of the LSTMs outputs'''
-    return K.exp(-K.sum(K.abs(left-right), axis=1, keepdims=True))
 
 # The visible layer
 left_input = Input(shape=(max_seq_length,), dtype='int32')
@@ -173,19 +208,26 @@ encoded_right = embedding_layer(right_input)
 # Since this is a siamese network, both sides share the same LSTM
 shared_lstm = LSTM(n_hidden)
 
-left_output = shared_lstm(encoded_left)
-right_output = shared_lstm(encoded_right)
+left_sequences = shared_lstm(encoded_left)
+right_sequences = shared_lstm(encoded_right)
+
+
+# attention layer
+left_output = left_sequences
+right_output = right_sequences
 
 # Calculates the distance as defined by the MaLSTM model
-malstm_distance = Merge(mode=lambda x: K.exp(-K.sum(K.abs(x[0]-x[1]), axis=1, keepdims=True)), output_shape=lambda x: (x[0][0], 1))([left_output, right_output])
+output = Merge(mode=lambda x: K.exp(-K.sum(K.abs(x[0]-x[1]), axis=1, keepdims=True)), output_shape=lambda x: (x[0][0], 1))([left_output, right_output])
+
 
 # Pack it all up into a model
-malstm = Model([left_input, right_input], [malstm_distance])
+malstm = Model([left_input, right_input], [output])
 
 # Adadelta optimizer, with gradient clipping by norm
 optimizer = Adadelta(clipnorm=gradient_clipping_norm)
 
 malstm.compile(loss='mean_squared_error', optimizer=optimizer, metrics=['accuracy'])
+
 
 # Start training
 training_start_time = time()
@@ -193,15 +235,17 @@ training_start_time = time()
 malstm_trained = malstm.fit([X_train['left'], X_train['right']], Y_train, batch_size=batch_size, epochs=n_epoch,
                             validation_data=([X_validation['left'], X_validation['right']], Y_validation))
 
-malstm_predict = malstm.predict([X_test['left'], X_test['right']])
-pickle.dump(malstm_predict, open(PRIDICT_FILE, "wb"))
+# malstm_predict = malstm.predict([X_test['left'], X_test['right']])
+# pickle.dump(malstm_predict, open(PRIDICT_FILE, "wb"))
+# del malstm_predict
+
+print("Training time finished.\n{} epochs in {}".format(n_epoch, datetime.timedelta(seconds=time()-training_start_time)))
+
 malstm.save(MODEL_FILE)
 del malstm
-del malstm_predict
 
 pickle.dump(malstm_trained.history, open(HISTORY_FILE, "wb"))
 del malstm_trained
-print("Training time finished.\n{} epochs in {}".format(n_epoch, datetime.timedelta(seconds=time()-training_start_time)))
 
 # Plot accuracy
 history = pickle.load(open(HISTORY_FILE, "rb"))
@@ -211,7 +255,7 @@ plt.title('Model Accuracy')
 plt.ylabel('Accuracy')
 plt.xlabel('Epoch')
 plt.legend(['Train', 'Validation'], loc='upper left')
-plt.savefig(PATH + "data/MaLSTM/accuracy.png")
+plt.savefig(ACC_PNG)
 plt.clf()
 
 # Plot loss
@@ -221,9 +265,9 @@ plt.title('Model Loss')
 plt.ylabel('Loss')
 plt.xlabel('Epoch')
 plt.legend(['Train', 'Validation'], loc='upper right')
-plt.savefig(PATH + "data/MaLSTM/loss.png")
+plt.savefig(LOSS_PNG)
 plt.clf()
 
-malstm = load_model(MODEL_FILE)
-malstm_predict = pickle.load(open(PRIDICT_FILE, "rb"))
-print(malstm_predict)
+# malstm = load_model(MODEL_FILE)
+# malstm_predict = pickle.load(open(PRIDICT_FILE, "rb"))
+# print(malstm_predict)
